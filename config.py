fc4be88d9068b7842c7398ec7e078674daf12082
x-kevinbro <damiruxox@outlook.com>
2025-07-16 10:37:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,21 +4,21 @@ load_dotenv()
 import os
 
 class BOT:
-    TOKEN = os.environ.get("TOKEN", "")
+    TOKEN = os.environ.get("TOKEN", "8145794920:AAHMHrOyl9LuKg6jNWQzUizTBe7N6hGe2VQ")
 
 class API:
-    HASH = os.environ.get("API_HASH", "")
+    HASH = os.environ.get("API_HASH", "771c072b4ae280c10508d636b494b285")
     ID = int(os.environ.get("API_ID", 0))
 
 class OWNER:
-    ID = int(os.environ.get("OWNER", 1773034985))
+    ID = int(os.environ.get("OWNER", 8046796637))
 
 class CHANNEL:
-    ID = int(os.environ.get("CHANNEL_ID", -1002732527335))
+    ID = int(os.environ.get("CHANNEL_ID", -1002804547624))
 
 class WEB:
     PORT = int(os.environ.get("PORT", 8080))
 
 class DATABASE:
-    URI = os.environ.get("DB_URI", "")
+    URI = os.environ.get("DB_URI", "mongodb+srv://damiruimo:ABVvGpyR8fVe7nar@x-kevin.83vo1.mongodb.net/")
     NAME = os.environ.get("DB_NAME", "Cluster0")
